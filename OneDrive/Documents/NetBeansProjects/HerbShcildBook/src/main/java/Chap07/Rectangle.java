e5699b7691bb921ce9def7484612f5e66dce5a7d
Keagan Dodsworth <keagan.dods@gmail.com>
2021-08-23 13:29:32 +0200

    commit message

--- OneDrive/Documents/NetBeansProjects/HerbShcildBook/src/main/java/Chap07/Rectangle.java
+++ OneDrive/Documents/NetBeansProjects/HerbShcildBook/src/main/java/Chap07/Rectangle.java
@@ -10,12 +10,32 @@ package Chap07;
  * @author keaga
  */
 public class Rectangle extends TwoDShape{
-    boolean isSquare() { 
-    if(width == height) return true; 
-    return false; 
-  } 
-   
-  double area() { 
-    return width * height; 
-  } 
+  // A default constructor.  
+  Rectangle() {  
+    super();  
+  }  
+  
+  // Constructor for Rectangle.  
+  Rectangle(double w, double h) {  
+    super(w, h, "rectangle"); // call superclass constructor  
+  }  
+  
+  // Construct a square.  
+  Rectangle(double x) {  
+    super(x, "rectangle"); // call superclass constructor  
+  }  
+  
+  // Construct an object from an object.  
+  Rectangle(Rectangle ob) {  
+    super(ob); // pass object to TwoDShape constructor  
+  }  
+  
+  boolean isSquare() {   
+    if(getWidth() == getHeight()) return true;   
+    return false;   
+  }   
+     
+  double area() {   
+    return getWidth() * getHeight();   
+  }   
 }
---
+++ OneDrive/Documents/NetBeansProjects/HerbShcildBook/src/main/java/Chap07/Sub1.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Chap07;
+
+/**
+ *
+ * @author keaga
+ */
+public class Sub1 extends Sup{
+    void who() { 
+    System.out.println("who() in Sub1"); 
+  } 
+}
